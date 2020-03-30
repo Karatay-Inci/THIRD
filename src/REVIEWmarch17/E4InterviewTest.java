e5f461e19d9dfab4c3cee8637a9ef2e4eed31999
inci-k <ikaratay@na.edu>
2020-03-29 23:53:40 -0500

    MARCH 29

---
+++ src/REVIEWmarch17/E4InterviewTest.java
@@ -0,0 +1,11 @@
+package REVIEWmarch17;
+
+public class E4InterviewTest {
+    public static void main(String[] args) {
+        E4Interview candidate1 = new E4Interview("Nihal",100,100,100);
+        System.out.println(candidate1.toString());
+        System.out.println(candidate1.decideResult());
+        candidate1.calculateScore();
+        System.out.println(candidate1.decideResult());
+    }
+}
