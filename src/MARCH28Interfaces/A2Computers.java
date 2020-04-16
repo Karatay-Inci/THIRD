d69a790a20eb410539ed7d16cb65e8da491bf42d
inci-k <ikaratay@na.edu>
2020-04-15 22:29:39 -0500

    APRIL 15

---
+++ src/MARCH28Interfaces/A2Computers.java
@@ -0,0 +1,8 @@
+package MARCH28Interfaces;
+
+public interface A2Computers {
+    void setMainBoard();
+    void installCPU();
+    void installMemory(int size);
+    void setWireless();
+}
---
+++ src/MARCH28Interfaces/A2LaptopTest.java
@@ -0,0 +1,7 @@
+package MARCH28Interfaces;
+
+public class A2LaptopTest {
+    public static void main(String[] args) {
+        A2Laptop myLaptop = new A2Laptop("intel fancy",64,3500,"Intel i9",20);
+    }
+}
