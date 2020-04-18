fb25f91e337ab6c5a1eb3ce6fdaf766b81be1964
inci-k <ikaratay@na.edu>
2020-04-18 08:28:12 -0500

    APRIL 18

--- src/MMAPRIL1Review/M2Test.java
+++ src/MMAPRIL1Review/M2Test.java
@@ -3,20 +3,20 @@ package MMAPRIL1Review;
 public class M2Test {
     public static void main(String[] args) {
         M2A ob1 = new M2A();
-        System.out.println(ob1.getA());
+        System.out.println("ob1 = "+ob1.getA());
 
         M2B ob2 = new M2B();
-        System.out.println(ob2.getA());
+        System.out.println("ob2 = "+ob2.getA());
 
         M2C ob3  = new M2C();
-        System.out.println(ob3.getA());
+        System.out.println("ob3 = "+ob3.getA());
 
         ob3.setA(40);
         ob2.setA(60);
         System.out.println();
-        System.out.println(ob1.getA());
-        System.out.println(ob2.getA());
-        System.out.println(ob3.getA());
+        System.out.println("ob1 = "+ob1.getA());
+        System.out.println("ob2 = "+ob2.getA());
+        System.out.println("ob3 = "+ob3.getA());
 
     }
 }
