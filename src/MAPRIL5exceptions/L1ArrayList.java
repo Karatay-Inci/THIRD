6e25ba89b58c0fb3a4765b6b30725d58e122d4c4
inci-k <ikaratay@na.edu>
2020-04-28 21:41:28 -0500

    APRIL 28

---
+++ src/MAPRIL5exceptions/L1ArrayList.java
@@ -0,0 +1,19 @@
+package MAPRIL5exceptions;
+
+import java.util.ArrayList;
+
+public class L1ArrayList {
+    public static void main(String[] args) {
+        ArrayList <String> first = new ArrayList<String>() ;
+        System.out.println(first);
+
+        first.add("Hello");
+        System.out.println(first);
+
+        first.add("World");
+        System.out.println(first);
+
+        first.add("JJJJ");
+        System.out.println(first);
+    }
+}
