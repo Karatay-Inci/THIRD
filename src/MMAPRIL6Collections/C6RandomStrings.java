a42bba58534a4ff1e9366806dbae5cdf38cd2d99
inci-k <ikaratay@na.edu>
2020-04-30 13:34:28 -0500

    APRIL 30

---
+++ src/MMAPRIL6Collections/C6RandomStrings.java
@@ -0,0 +1,8 @@
+package MMAPRIL6Collections;
+
+public class C6RandomStrings {
+    public static void main(String[] args) {
+        String letters="abcdefghijklmnopqrstvuyz ";
+        //System.out.println(letters.charAt((int)(Math.random()*letters.length())));
+    }
+}
