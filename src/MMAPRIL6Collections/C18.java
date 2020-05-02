3e5fb53d11bea766013a50b16a5b9a73118edd71
inci-k <ikaratay@na.edu>
2020-05-02 00:22:11 -0500

    MAY 1

--- src/MMAPRIL6Collections/C18.java
+++ src/MMAPRIL6Collections/C18.java
@@ -1,5 +1,8 @@
 package MMAPRIL6Collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class C18 {
     /*Write a java program to shift left number of elements
     Sample input
@@ -7,4 +10,28 @@ public class C18 {
                            Shift(2)
     Output
                      3,4,5*/
+    public static void main(String[] args) {
+        int [] myArr={1,2,3,4,5,6,7,8,9};
+        System.out.println(Arrays.toString(removeFromStart(myArr,5)));
+        int [] abc={};
+        removeFromStart(abc,3);
+    }
+    public static int[] removeFromStart(int[] arr, int numberOfElements){
+        ArrayList<Integer> result = new ArrayList<>();
+        if (arr.length>0){
+            for (int i = numberOfElements; i <arr.length ; i++) {
+                result.add(arr[i]);
+            }
+        }
+        else{
+            System.out.println(" It is empty array");
+        }
+          int[] realResult = new int [result.size()];
+        int index = 0;
+        for (int num : result){
+            realResult[index]=num;
+            index++;
+        }
+        return realResult;
+    }
 }
