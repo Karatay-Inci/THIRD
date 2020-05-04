edb4fe68db8d73df19eb50fa7ba86848613f9551
inci-k <ikaratay@na.edu>
2020-05-04 12:58:33 -0500

    MAY 4

--- src/MMAPRIL6Collections/D6StringArrayList.java
+++ src/MMAPRIL6Collections/D6StringArrayList.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class D6StringArrayList {
+    //Create 2 dimensional String Array List and initialize
+    // Write a function to find number of uppercase letters in every row
     public static void main(String[] args) {
         ArrayList<ArrayList<String >> words = new ArrayList<>();
-        ArrayList<String>               a   = new ArrayList<>(Arrays.asList("Java","is","fun"));
+        ArrayList<String>               a   = new ArrayList<>(Arrays.asList("JaVa","is","Fun"));
         words.add(a);
         words.add(a);
         words.get(1).add("AAAAA");
         System.out.println(words);
         findUpperCases(words);
+        System.out.println("\t\t----");
         findLowerCases(words);
+        System.out.println("\t\t----");
+        findUpperCasesInWords(words);
 
     }
     public static void findUpperCases(ArrayList<ArrayList<String >>arr){
@@ -29,9 +34,10 @@ public class D6StringArrayList {
                 }
                 counterRow+=counterWord;
             }
-            System.out.println("There are "+counterRow+"upper case at row"+(i+1));
+            System.out.println("There are "+counterRow+" upper case at row"+(i+1));
         }
     }
+    // Write a function to find number of lower case letters in every row
     public static void findLowerCases(ArrayList<ArrayList<String >>arr){
         int counterWord;
         int counterRow;
@@ -46,7 +52,25 @@ public class D6StringArrayList {
                 }
                 counterRow+=counterWord;
             }
-            System.out.println("There are "+counterRow+"upper case at row"+(i+1));
+            System.out.println("There are "+counterRow+" lower case at row"+(i+1));
+        }
+    }
+    // Write a function to find number of uppercase letters in every word
+    public static void findUpperCasesInWords(ArrayList<ArrayList<String >> arr){
+        int countWord;
+        for (int i = 0; i <arr.size() ; i++) {
+            for (int j = 0; j <arr.get(i).size() ; j++) {
+                countWord=0;
+                for (int k = 0; k <arr.get(i).get(j).length() ; k++) {
+                    if(Character.isUpperCase(arr.get(i).get(j).charAt(k))){
+                        countWord++;
+                    }
+                }
+                System.out.println("There are "+countWord+" upper case in "+arr.get(i).get(j));
+
+
+            }
+
         }
     }
 }
