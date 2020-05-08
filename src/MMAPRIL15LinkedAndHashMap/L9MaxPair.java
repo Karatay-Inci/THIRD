7b74d2e84d8e094ef230983ee9915139169fa120
inci-k <ikaratay@na.edu>
2020-05-08 04:00:49 -0500

    MAY 8

---
+++ src/MMAPRIL15LinkedAndHashMap/L9MaxPair.java
@@ -0,0 +1,29 @@
+package MMAPRIL15LinkedAndHashMap;
+
+import java.util.HashMap;
+
+public class L9MaxPair {
+    //Write a java program to find max multiplication of adjacent values of the integer array.
+    //Return index and values
+    public static void main(String[] args) {
+        int arr[]={1,2,3,5,2,4};
+        findAdjacentMax(arr);
+    }
+    public static void findAdjacentMax(int []arr){
+        HashMap<Integer, Integer> hm =  new HashMap<>();
+        for (int i = 0; i <arr.length-1 ; i++) {
+            hm.put(i,arr[i]*arr[i+1]);
+        }
+       // System.out.println(hm);
+        int max=0;
+        int index=0;
+        for (int key : hm.keySet()){
+            if (hm.get(key)>max){
+                max=hm.get(key);
+                index=key;
+            }
+        }
+        System.out.println(index+"\t=\t"+max);
+    }
+
+}
