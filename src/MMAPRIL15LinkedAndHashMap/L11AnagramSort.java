3beb7b1cf9566868f8f4db8b2de3692e140f7cbe
inci-k <ikaratay@na.edu>
2020-05-08 15:25:15 -0500

    MAY 8

---
+++ src/MMAPRIL15LinkedAndHashMap/L11AnagramSort.java
@@ -0,0 +1,30 @@
+package MMAPRIL15LinkedAndHashMap;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class L11AnagramSort {
+    public static void main(String[] args) {
+        String word1 = "abcd";
+        String word2 = "dcba";
+        char[] chars1 = word1.toCharArray();
+        char[] chars2 = word2.toCharArray();
+        Arrays.sort(chars1);
+        System.out.println(Arrays.toString(chars1));
+        Arrays.sort(chars2);
+        System.out.println(Arrays.toString(chars2));
+        boolean isAnagram = true;
+        for (int i = 0; i <chars1.length ; i++) {
+            if (chars1[i]!=chars2[i]){
+                isAnagram=false;
+                break;
+            }
+        }
+        if (isAnagram){
+            System.out.println("Anagram");
+        }
+        else {
+            System.out.println("Not Anagram");
+        }
+    }
+}
---
+++ src/MMAPRIL15LinkedAndHashMap/L12.java
@@ -0,0 +1,8 @@
+package MMAPRIL15LinkedAndHashMap;
+
+public class L12 {
+    //Write a java method to check given key is exist or not
+    public static void main(String[] args) {
+
+    }
+}
