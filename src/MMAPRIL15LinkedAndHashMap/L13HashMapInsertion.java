ad232c71f9af9f7a92c0e165c715cad3918a505a
inci-k <ikaratay@na.edu>
2020-05-09 06:48:27 -0500

    MAY 8

---
+++ src/MMAPRIL15LinkedAndHashMap/L13HashMapInsertion.java
@@ -0,0 +1,15 @@
+package MMAPRIL15LinkedAndHashMap;
+
+import java.util.HashMap;
+
+public class L13HashMapInsertion {
+    public static void main(String[] args) {
+        HashMap<Integer,Integer> hm = new HashMap<>(10);
+        for (int i = 0; i <10 ; i++) {
+            hm.put(i,i+i);
+        }
+        for (int x:hm.keySet()) {
+            System.out.println(x+" = "+hm.get(x));
+        }
+    }
+}
