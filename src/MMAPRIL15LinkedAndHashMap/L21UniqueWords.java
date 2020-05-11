801b7d769040a09c8279c63a00d6435552506737
inci-k <ikaratay@na.edu>
2020-05-11 05:32:11 -0500

    MAY 11

--- src/MMAPRIL15LinkedAndHashMap/L21UniqueWords.java
+++ src/MMAPRIL15LinkedAndHashMap/L21UniqueWords.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class L21UniqueWords {
+    //Find the distinct words in a given sentence
+    //  Input "java is java"
+    //  Output Java , is
     public static void main(String[] args) {
         String sentence ="can a fish fish a fish in a fish can";
         String [] words = sentence.split(" ");
